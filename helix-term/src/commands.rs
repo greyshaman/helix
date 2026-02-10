a07279d44e857de68cd8cb1d324b888d4f85eb58
Sergey Reshetnikov <shaman@simmirra.com>
2026-02-10 20:22:05 +0300

    disable get session id until glibc::getsid() not supported by redox

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -6507,7 +6507,11 @@ fn suspend(_cx: &mut Context) {
     {
         // SAFETY: These are calls to standard POSIX functions.
         // Unsafe is necessary since we are calling outside of Rust.
+        #[cfg(not(target_os = "redox"))]
         let is_session_leader = unsafe { libc::getpid() == libc::getsid(0) };
+        // FIXME: remove this block when redox will support getsid()
+        #[cfg(target_os = "redox")]
+        let is_session_leader = true;
 
         // If helix is the session leader, there is nothing to suspend to, so skip
         if is_session_leader {
